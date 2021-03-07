22f2dc82445b9f2985f794420ba9bec1bcc720e5
laercio <laercioigorps@gmail.com>
2021-03-06 21:22:15 -0300

    polls detail and vote implemented without tests

--- polls/views.py
+++ polls/views.py
@@ -4,4 +4,27 @@ from .models import Question, Choice
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello world!")
+	question_list = Question.objects.all()
+
+	context = {
+
+		"list_questions" : question_list,
+	}
+	return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+	question = Question.objects.get(id=question_id)
+
+	choice_list = question.choice_set.all()
+
+	context = {
+
+		"list_choices" : choice_list,
+		"question" : question,
+	}
+	return render(request, "polls/detail.html", context)
+
+def vote(request, question_id):
+	question = Question.objects.get(id=question_id)
+	choice_id = question.choice_set.get(id= request.POST['choice'])
+	return HttpResponse("sua escolha foi %s" % str(choice_id.id))
